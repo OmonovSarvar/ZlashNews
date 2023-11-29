e7ce38a4ff6ac1fa2c56f0f6921be8a179b5d0a3
OmonovSarvar <sarvaromon33@gmail.com>
2023-11-29 20:52:49 +0500

    Day of the week added for showing days

--- zlash_news_api/views.py
+++ zlash_news_api/views.py
@@ -17,6 +17,10 @@ class NewsList(generics.ListAPIView):
     serializer_class = NewsSerializer
 
     def get_queryset(self):
+        # Get day of the week
+        day = date.today()
+        x = day_name[day.weekday()]
+
         url1 = 'https://www.bbc.com/news'  # Get news from this links
         url2 = 'https://eurasianet.org'  # Get news from this links
 
