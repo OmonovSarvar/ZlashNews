f2ed7a26769acdbf6ba7f149ada2cb81f91abd51
OmonovSarvar <sarvaromon33@gmail.com>
2023-11-29 20:49:53 +0500

    get queryset function added for NewsList views.py

--- zlash_news_api/views.py
+++ zlash_news_api/views.py
@@ -15,3 +15,18 @@ from .serializers import NewsSerializer
 class NewsList(generics.ListAPIView):
     queryset = NewsModel.objects.all()
     serializer_class = NewsSerializer
+    def get_queryset(self):
+        # Get day of the week
+        day = date.today()
+        x = day_name[day.weekday()]
+
+        url1 = 'https://www.bbc.com/news'  # Get news from this links
+        url2 = 'https://eurasianet.org'  # Get news from this links
+
+        # request to first site
+        news_list = []
+        response = requests.get(url1)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        result = soup.find(id='orb-modules')
+        title = result.find_all('h3', class_='gs-c-promo-heading__title gel-pica-bold nw-o-link-split__text')
+        content = result.find_all('p', class_='gs-c-promo-summary gel-long-primer gs-u-mt nw-c-promo-summary')
